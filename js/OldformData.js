b4bd3668c0cdc4278e8b835158acedc247c3ea88
debasish-barman-0011 <db.myjob.2023@gmail.com>
2024-09-09 09:47:32 +0530

    Form Data Receiving Conflict Resolved

--- js/OldformData.js
+++ js/OldformData.js
@@ -1,65 +1,50 @@
-// Script for taking userinputs to the google sheets //
-
-// Google Sheet API Key //
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbyRzJrRVLwecLbAB6tNRvNUiWRwKgoLL2S6Wv3tQW8iSt645zYBd3b8NJ3XOc6Ym7Kf5g/exec";
-
-// Form Reference from HTML file //
 const form = document.forms["submit-to-google-sheet"];
-
-form.addEventListener("submit", handleSubmit);
-// Ensure form is not empty //
-function handleSubmit(e) {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  if (!isFormValid()) {
-    showErrorAlert("Please fill out all required fields.");
-    return;
-  }
-
-  showConfirmationAlert();
-}
-// validate the form as all the fields are required //
-function isFormValid() {
-  const requiredFields = form.querySelectorAll("[required]");
-  return Array.from(requiredFields).every((field) => field.value.trim() !== "");
-}
-// Form Submission Logic //
-function submitForm() {
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
     .then((response) => {
       if (response.ok) {
         showSuccessAlert();
-        form.reset();
       } else {
-        throw new Error("Server error");
+        showErrorAlert("Internal Server Error, Please try again.");
       }
+
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 4000);
+      form.reset();
     })
     .catch((error) => {
       console.error("Error!", error.message);
-      showErrorAlert(" Please try again.");
+      showErrorAlert("An error occurred. Please try again.");
     });
-}
+});
 
+document.getElementById("btnSubmit").addEventListener("click", function (e) {
+  e.preventDefault();
+  showConfirmationAlert();
+});
 function showConfirmationAlert() {
   Swal.fire({
-    title: "Post Your Message 🤔",
+    title: "Send Your Message?",
     text: "",
-    icon: "question",
+    icon: "info",
     showCancelButton: true,
     confirmButtonColor: "#3085d6",
     cancelButtonColor: "#d33",
     confirmButtonText: "Send",
   }).then((result) => {
     if (result.isConfirmed) {
-      submitForm();
+      form.dispatchEvent(new Event("submit"));
     }
   });
 }
 
 function showSuccessAlert() {
   Swal.fire({
-    title: "DONE 😊",
+    title: "DONE",
     text: "Thank You.",
     icon: "success",
     timer: 2000,
@@ -69,9 +54,9 @@ function showSuccessAlert() {
 
 function showErrorAlert(errorMessage) {
   Swal.fire({
-    title: "Failed 😣",
-    text: errorMessage,
+    title: "Failed!",
+    text: "Internal Server Error!",
     icon: "error",
-    confirmButtonText: "OK",
+    confirmButtonText: "Try Again",
   });
 }
